9049fe433216712663b518846855ca8647bac3f8
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 11:16:04 +0100

    Updates exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,7 +1,6 @@
 import pytest
-from src.exercise import main
+from src.exercise import greatest
 
-def test_exercise(capsys):
-    main()
-    out, err = capsys.readouterr()
-    assert out == "\n", "Should read ''"
+def test_exercise():
+    assert greatest(3,2,6) == 6
+    assert greatest(2,7,4) == 7
